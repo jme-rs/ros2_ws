f7084aaf03efb10440e6bcb68c507241ba10ad4d
jme-rs <jme.co.jp@gmail.com>
2024-03-06 17:27:25 +0900

    新しいノードを追加しました

--- src/nav2_test/nav2_test/key_input_pub.py
+++ src/nav2_test/nav2_test/key_input_pub.py
@@ -3,16 +3,16 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class PointSub(Node):
+class KeyInputPub(Node):
     def __init__(self):
-        super().__init__("point_sub")
-        self.publisher_ = self.create_publisher(String, "point_", 10)
-        timer_period = 2.0  # Frequency of publishing
+        super().__init__("key_input_pub")
+        self.publisher_ = self.create_publisher(String, "key_input", 10)
+        timer_period = 1.0  # Frequency of publishing
         self.timer = self.create_timer(timer_period, self.pub_callback)
 
     def pub_callback(self):
         msg = String()
-        msg.data = "HelloWorld!!"
+        msg.data = input("Enter a point: ")
         self.publisher_.publish(msg)
         self.get_logger().info("Publishing: {}".format(msg))
 
@@ -20,7 +20,7 @@ class PointSub(Node):
 def main(args=None):
     try:
         rclpy.init(args=args)
-        talker = PointSub()
+        talker = KeyInputPub()
         rclpy.spin(talker)
     except KeyboardInterrupt:
         pass
