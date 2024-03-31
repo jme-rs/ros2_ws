f8425a0c0db25dad3c4aa7344affa2ec8e076129
Hajime Sasaki <109297897+jme-rs@users.noreply.github.com>
2024-04-01 02:30:15 +0900

    display_node.py を更新

--- src/display_node/display_node/display_node.py
+++ src/display_node/display_node/display_node.py
@@ -10,11 +10,11 @@ class DisplayNode(Node):
         self.subscription = self.create_subscription(
             Image,
             topic_name,
-            self.__listener_callback,
+            self._listener_callback,
             10)
-        self.__cv_bridge = CvBridge()
+        self._cv_bridge = CvBridge()
 
-    def __listener_callback(self, image: Image):
+    def _listener_callback(self, image: Image):
         self.get_logger().info('Image received')
-        cv_image = self.__cv_bridge.imgmsg_to_cv2(image)
+        cv_image = self._cv_bridge.imgmsg_to_cv2(image)
         cv2.imshow('DisplayNode', cv_image)
